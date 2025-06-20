6d8589f6bd6f266a50f7cf0f793dc88e76d2848f
vanvo299 <luongvanvo29@gmail.com>
2025-06-20 23:35:40 +0700

    init folder

---
+++ server.js
@@ -0,0 +1,13 @@
+import app from './src/app.js';
+import dotenv from 'dotenv'
+
+dotenv.config()
+const PORT = process.env.PORT
+const server = app.listen(PORT, () => {
+    console.log(`Web Service eCommerce start with port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+})
+
+process.on('SIGINT', () => {
+    server.close( () => console.log(`Exit Server Express`)) 
+})
---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import compression from 'compression'
+
+const app = express()
+
+// INIT MIDDLEWARES
+
+// morgan dùng để log request
+app.use(morgan("dev"))
+
+// helmet dùng để bảo mật ứng dụng express
+// nó sẽ thiết lập các tiêu đề HTTP bảo mật
+// để giúp bảo vệ ứng dụng khỏi các lỗ hổng bảo mật phổ biến
+app.use(helmet())
+// compression dùng để nén các phản hồi HTTP
+// giúp giảm kích thước dữ liệu được truyền qua mạng
+// từ đó cải thiện hiệu suất và tốc độ tải trang
+// nén các phản hồi HTTP
+// giúp giảm băng thông và thời gian tải trang
+app.use(compression())
+
+// init db
+
+// init routes
+app.get('/', (req, res) => {
+    const strCompress = 'Hello VoLuong'
+    return res.status(200).send({ 
+        message: "Welcome Everyone ",
+        metadata: strCompress.repeat(10000)
+     })
+})
+
+// handle error
+
+export default app
